90719c40f68854df3b90bdca3c565054714b2d0a
Daniel Couto <54581486+DarKouto@users.noreply.github.com>
2024-12-17 15:50:49 +0000

    Update BP.py
    change variables

--- BP.py
+++ BP.py
@@ -1,21 +1,21 @@
 print("Desconto BP Poupa Mais em Python por DarKouto. V1.0\n");
 
-valorSusana = float(input("Quanto combustível meteu a Susana? (€) "))
+valor1 = float(input("Quanto combustível meteu o Condutor 1? (€) "))
 valorTotal = float(input("Qual foi o valor total sem desconto? (€) "))
 valorFinal = float(input("Qual foi o preço final com desconto? (€) "))
 
-valorDaniel = (valorTotal-valorSusana)
-percentagemSusana = (100*valorSusana)/valorTotal
-percentagemDaniel = (100*valorDaniel)/valorTotal
-contaSusana = (percentagemSusana*valorFinal)/100
-contaDaniel = (percentagemDaniel*valorFinal)/100
+valor2 = (valorTotal-valor1)
+percentagem1 = (100*valor1)/valorTotal
+percentagem2 = (100*valor2)/valorTotal
+conta1 = (percentagem1*valorFinal)/100
+conta2 = (percentagem2*valorFinal)/100
 
 print(
-    "\nA Susana meteu:",round(valorSusana,2),"€",
-    "\nO Daniel meteu:",round(valorDaniel,2),"€",
-    "\nPercentagem Susana:",round(percentagemSusana,2),"%",
-    "\nPercentagem Daniel:",round(percentagemDaniel,2),"%",
-    "\nSusana paga:",round(contaSusana,2),"€",
-    "\nDaniel paga:",round(contaDaniel,2),"€")
+    "\nA Condutor 1 meteu:",round(valor1,2),"€",
+    "\nO Condutor 2 meteu:",round(valor2,2),"€",
+    "\nPercentagem Condutor 1:",round(percentagem1,2),"%",
+    "\nPercentagem Condutor 2:",round(percentagem2,2),"%",
+    "\nCondutor 1 paga:",round(conta1,2),"€",
+    "\nCondutor 2 paga:",round(conta2,2),"€")
 
 sair = input("\nPrima ENTER para sair")
